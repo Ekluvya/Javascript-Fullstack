3abf6aef7d4cb151d2ad97d37ae81b0da92c7fd0
ekluvya <47681673+Ekluvya@users.noreply.github.com>
2025-04-28 13:59:39 +0530

    Completed React Crash Course

--- 03. React Crash Course/react-jobs-2025/src/components/JobListings.jsx
+++ 03. React Crash Course/react-jobs-2025/src/components/JobListings.jsx
@@ -1,19 +1,47 @@
-import jobs from "../jobs.json";
+import { useEffect, useState } from "react";
 import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
-const JobListings = () => {
-  const recentJobs = jobs.slice(0, 3);
+const JobListings = ({ isHome = false }) => {
+  const [jobListings, setJobListings] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobListings = async () => {
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error("Failed to fetch job listings");
+        }
+        const data = await response.json();
+        setJobListings(data);
+      } catch (error) {
+        console.error("Error fetching job listings:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobListings();
+  }, []);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {isHome ? "Recent Jobs" : "Job Listings"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {recentJobs.map((job) => (
-            <JobListing key={job.id} job={job} />
-          ))}
-        </div>
+
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobListings.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
